415ec2710c89a0444384d923ed84e503904d0910
Carlos Ospina <camoa2005@gmail.com>
2024-08-28 13:25:37 -0500

    Adding config overlay and other updates

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -435,8 +435,8 @@ class ComposerStaticInit5c7f23189dfdbae478d03c4985419aea
         ),
         'Doctrine\\Common\\' => 
         array (
-            0 => __DIR__ . '/..' . '/doctrine/common/src',
-            1 => __DIR__ . '/..' . '/doctrine/event-manager/src',
+            0 => __DIR__ . '/..' . '/doctrine/event-manager/src',
+            1 => __DIR__ . '/..' . '/doctrine/common/src',
         ),
         'Dflydev\\DotAccessData\\' => 
         array (
@@ -505,10 +505,6 @@ class ComposerStaticInit5c7f23189dfdbae478d03c4985419aea
         ),
     );
 
-    public static $fallbackDirsPsr0 = array (
-        0 => __DIR__ . '/..' . '/pear/pear-core-minimal/src',
-    );
-
     public static $classMap = array (
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'CURLStringFile' => __DIR__ . '/..' . '/symfony/polyfill-php81/Resources/stubs/CURLStringFile.php',
@@ -536,6 +532,10 @@ class ComposerStaticInit5c7f23189dfdbae478d03c4985419aea
         'Drupal\\Core\\Installer\\InstallerRedirectTrait' => __DIR__ . '/../..' . '/core/lib/Drupal/Core/Installer/InstallerRedirectTrait.php',
         'Drupal\\Core\\Site\\Settings' => __DIR__ . '/../..' . '/core/lib/Drupal/Core/Site/Settings.php',
         'Normalizer' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php',
+        'OS_Guess' => __DIR__ . '/..' . '/pear/pear-core-minimal/src/OS/Guess.php',
+        'PEAR' => __DIR__ . '/..' . '/pear/pear-core-minimal/src/PEAR.php',
+        'PEAR_Error' => __DIR__ . '/..' . '/pear/pear-core-minimal/src/PEAR.php',
+        'PEAR_ErrorStack' => __DIR__ . '/..' . '/pear/pear-core-minimal/src/PEAR/ErrorStack.php',
         'PEAR_Exception' => __DIR__ . '/..' . '/pear/pear_exception/PEAR/Exception.php',
         'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
         'ReturnTypeWillChange' => __DIR__ . '/..' . '/symfony/polyfill-php81/Resources/stubs/ReturnTypeWillChange.php',
@@ -556,6 +556,7 @@ class ComposerStaticInit5c7f23189dfdbae478d03c4985419aea
         'SebastianBergmann\\Diff\\Parser' => __DIR__ . '/..' . '/sebastian/diff/src/Parser.php',
         'SebastianBergmann\\Diff\\TimeEfficientLongestCommonSubsequenceCalculator' => __DIR__ . '/..' . '/sebastian/diff/src/TimeEfficientLongestCommonSubsequenceCalculator.php',
         'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'System' => __DIR__ . '/..' . '/pear/pear-core-minimal/src/System.php',
         'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
         'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
@@ -566,7 +567,6 @@ class ComposerStaticInit5c7f23189dfdbae478d03c4985419aea
             $loader->prefixLengthsPsr4 = ComposerStaticInit5c7f23189dfdbae478d03c4985419aea::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit5c7f23189dfdbae478d03c4985419aea::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit5c7f23189dfdbae478d03c4985419aea::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInit5c7f23189dfdbae478d03c4985419aea::$fallbackDirsPsr0;
             $loader->classMap = ComposerStaticInit5c7f23189dfdbae478d03c4985419aea::$classMap;
 
         }, null, ClassLoader::class);
