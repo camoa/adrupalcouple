33197491dc9ab3019fcc28eb014fcbfa8349d9cc
Carlos Ospina <camoa2005@gmail.com>
2024-10-23 17:34:13 -0500

    mend

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -761,6 +761,8 @@ $settings['entity_update_backup'] = TRUE;
  */
 $settings['migrate_node_migrate_type_classic'] = FALSE;
 
+$settings['config_exclude_modules'] = ['metatag_ai'];
+
 // Automatically generated include for settings managed by ddev.
 if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include __DIR__ . '/settings.ddev.php';
